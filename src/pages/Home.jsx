eacfc5a1be7afce598221a25735ea928c1cd87b9
Shwetanshu13 <shwetanshusinha13@gmail.com>
2024-03-06 10:50:40 +0530

    Formula rebalnce corrected

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
           />
           <BudgetCard
             text="Balance after rebalancing Investments and Loans"
-            amount={totalIncome - totalExpense - totalLoan + totalInvestment}
+            amount={totalIncome - totalExpense}
             colour="green"
           />
           <BudgetCard text="Income" amount={totalIncome} colour="green" />
