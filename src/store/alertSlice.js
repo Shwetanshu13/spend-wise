232cdc18074948211c923ecaf9e5ef1f3e4f1892
Shwetanshu13 <shwetanshusinha13@gmail.com>
2024-10-08 00:18:01 +0530

    Refactor alertSlice.js reducer functions

--- src/store/alertSlice.js
+++ src/store/alertSlice.js
@@ -1,26 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    alertVal:0
+  alertVal: 0,
 };
 
 const alertSlice = createSlice({
-    name: "alert",
-    initialState,
-    reducers: {
-        added: (state) => {
-            state.alertVal = 1;
-        },
-        deleted: (state) => {
-            state.alertVal = 2;
-        },
-        error: (state) => {
-            state.alertVal = 3;
-        },
-        reset: (state) => {
-            state.alertVal = 0;
-        },
+  name: "alert",
+  initialState,
+  reducers: {
+    added: (state) => {
+      state.alertVal = 1;
     },
+    deleted: (state) => {
+      state.alertVal = 2;
+    },
+    error: (state) => {
+      state.alertVal = 3;
+    },
+    reset: (state) => {
+      state.alertVal = 0;
+    },
+  },
 });
 
 export const { added, deleted, error, reset } = alertSlice.actions;
